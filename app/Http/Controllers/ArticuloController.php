3047314834ff2b075a7c5f4b409e91765c716d15
Nicolas <nicolashilarion420@gmail.com>
2022-03-12 13:49:33 -0500

    Apirestful laravel para pruebas con cliente

---
+++ app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Articulo;
+use Illuminate\Http\Request;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articulos = Articulo::all();
+        return $articulos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $articulos = new Articulo();
+        $articulos->descripcion = $request->descripcion;
+        $articulos->precio = $request->precio;
+        $articulos->stock = $request->stock;
+
+        $articulos->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $articulos = Articulo::findOrFail($request->id);
+        $articulos->descripcion = $request->descripcion;
+        $articulos->precio = $request->precio;
+        $articulos->stock = $request->stock;
+
+        $articulos->save();
+        return $articulos;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($request)
+    {
+        $articulos = Articulo::destroy($request->id);
+        return $articulos;
+    }
+}
